2b51388b4d51571c06befb855ceef80cd3f2077b
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2023-10-08 15:46:26 -0700

    the last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -21,7 +21,7 @@ printf("the last digit of %d is %d and is greater than 5\n", n, ld);
 }
 else if (ld < 6)
 {
-printf("the last digit of %d is %d and is less than 5 and not 0\n", n, ld);
+printf("the last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 }
 else if (ld == 0)
 {
