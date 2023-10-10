c9dc42eefce6d4241dcb0118d9591f9e5499b935
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2023-10-10 16:41:02 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,25 +1,30 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * print_sign - prints the sign of a number
- * @n: the int to check
- * Return: 1 and prints + if n is greater than zero
- * 0 and prints 0 if n is zero
- * -1 and prints - if n is less than zero
- */
+ * print_sign - function is the master function to check the lowercase char
+ *
+ * @n: check the input
+ *
+ * Return: Always 0 (success)
+*/
+
 int print_sign(int n)
 {
 	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}  else if (n == 0)
+{
+	_putchar('+');
+	return (1);
 }
-		_putchar(48);
-		return (0);
-	}  else if (n < 0)
-	{
-		_putchar('-');
-	}
-		return (-1);
+	else if (n < 0)
+{
+	_putchar('-');
+	return (-1);
+}
+	else if (n == 0)
+{
+	_putchar('0');
+	return (0);
+}
+	return (0);
 }
