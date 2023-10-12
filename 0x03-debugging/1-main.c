74943027e1ddddbc175c89ae0066f3d5c6aad685
Muna Elmetwally <munaelmetwally2397@gmail.com>
2023-10-13 00:59:17 +0300

    yala

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,6 +2,7 @@
 
 /**
 * main - causes an infinite loop
+*
 * Return: 0
 */
 
@@ -13,7 +14,7 @@ printf("Infinite loop incoming :(\n");
 
 i = 0;
 /**
- * while (i < 10)
+* while (i < 10)
 *{
 *putchar(i);
 *}*/
