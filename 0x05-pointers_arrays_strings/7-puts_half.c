a25d7f128c644029d9a0c67b8e4a1d4ddd21300b
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2023-10-16 16:39:09 -0700

    main

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int t, n, i;
+
+	t = 0;
+
+	while (str[t] != '\0')
+	{
+		t++;
+	}
+
+	if (t % 2 == 0)
+	{
+		for (i = t / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	} else if (t % 2)
+	{
+		for (n = t - 1) / 2; n < t - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+	_putchar('\n');
+}
