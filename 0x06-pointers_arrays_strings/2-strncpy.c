78293379f7c4f3dd0a8aef7ba99328698fc36ec6
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2023-10-19 14:32:11 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,15 +6,13 @@
 * @n: size of second string
 * Return: Always 0.
 */
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n)
 {
-int i;
-for (i = 0 ; i < n && src[i] != '\0' ; i++)
-dest[i] = src[i];
-while (i < n)
-{
-dest[i] = '\0';
-i++;
-}
-return (dest);
+  int i;
+
+  for (i = 0; i < n && src[i] != '\0'; i++)
+    *(dest + i) = *(src + i);
+  for ( ; i < n; i++)
+    *(dest + i) = '\0';
+  return (dest);
 }
