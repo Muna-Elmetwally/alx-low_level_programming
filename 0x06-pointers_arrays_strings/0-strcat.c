527b0ae31c806fa60a5c9f7ab950c50e675b0b64
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2023-10-20 01:47:36 +0300

    b

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strcat - concatenates 2 strings.
  * @dest: string with concatenation
@@ -8,22 +7,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	int ch1, ch2;
+	int lengthD, lengthS;
 
-	ch1 = 0;
-	ch2 = 0;
+	lengthD = 0;
+	lengthS = 0;
 
-	int i;
+	while (*(dest + lengthD) != '\0')
+		lengthD++;
 
-	while (*(dest + ch1) != '\0')
-		ch1++;
-
-	while (*(src + ch1) != '\0' && ch1 < 97)
+	while (*(src + lengthS) != '\0' && lengthD < 97)
 	{
-		*(dest + ch1) = *(src + ch2);
-		ch1++;
-		ch2++;
+		*(dest + lengthD) = *(src + lengthS);
+		lengthD++;
+		lengthS++;
 	}
-	*(dest + ch2) = '\0';
+	*(dest + lengthD) = '\0';
 	return (dest);
 }
