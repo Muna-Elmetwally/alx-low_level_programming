9bce4f5b401a9aca1f36f7dbe6569db86388660c
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2023-10-27 18:00:28 -0700

    100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,28 +8,28 @@
 * @x: Passed in variable from main for calculations
 * Return: The number of coins needed minimum for the passed in variable
 */
-int coinConverter(int x)
+int coinConverter(int i)
 {
 int count = 0;
-while (x != 0)
+while (i != 0)
+}
+if (i % 10 == 9 || i % 10 == 7)
+i -= 2;
+else if (i % 25 == 0)
+i -= 25;
+else if (i % 10 == 0)
+i -= 10;
+else if (i % 5 == 0)
+i -= 5;
+else if (i % 2 == 0)
 {
-if (x % 10 == 9 || x % 10 == 7)
-x -= 2;
-else if (x % 25 == 0)
-x -= 25;
-else if (x % 10 == 0)
-x -= 10;
-else if (x % 5 == 0)
-x -= 5;
-else if (x % 2 == 0)
-{
-if (x % 10 == 6)
-x -= 1;
+if (i % 10 == 6)
+i -= 1;
 else
-x -= 2;
+i -= 2;
 }
 else
-x -= 1;
+i -= 1;
 count++;
 }
 return (count);
@@ -42,19 +42,19 @@ return (count);
 */
 int main(int argc, char *argv[])
 {
-int x, coin;
+int i, coin;
 coin = 0;
 if (argc != 2)
 {
 printf("Error\n");
 return (1);
 }
-x = atoi(argv[1]);
-if (x < 0)
+i = atoi(argv[1]);
+if (i < 0)
 printf("0\n");
 else
 {
-coin = coinConverter(x);
+coin = coinConverter(i);
 printf("%d\n", coin);
 }
 return (0);
