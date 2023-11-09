e5701af46ef334a8115af09605c8f627e35e6d4d
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2023-11-09 13:35:08 -0800

    3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,50 +1,96 @@
-#include "variadic_functions.h"
 #include <stdio.h>
+#include "variadic_functions.h"
 #include <stdarg.h>
+
 /**
- * print_all - Entry Point
- * c = char, i = int, f = float, s = char * (if null print (nil))
- * @format: list of arg types
- * Return: 0
+ * op_c - Print character .
+ * @form: name va_list
+ * Return: Nothing.
  */
-void print_all(const char * const format, ...)
+
+void op_c(va_list form)
 {
-	va_list valist;
-	int y = 0, x = 0;
-	char *sep = ", ";
+	printf("%c", va_arg(form, int));
+}
+/**
+ * op_i - Print Integer
+ * @form: name va_list
+ * Return: Nothing.
+ */
+
+void op_i(va_list form)
+{
+	printf("%i", va_arg(form, int));
+}
+/**
+ * op_f - print FLoat numbers
+ * @form: name of va_list
+ *
+ * Return: Nothing.
+ */
+
+void op_f(va_list form)
+{
+	printf("%f", va_arg(form, double));
+}
+/**
+ * op_s -print string
+ * @form: name va_list
+ * Return: Nothing.
+ */
+
+void op_s(va_list form)
+{
 	char *str;
 
-	va_start(valist, format);
-
-	while (format && format[x])
-		x++;
-
-	while (format && format[y])
+	str = va_arg(form, char *);
+	if (str == NULL)
 	{
-		if (y  == (x - 1))
-		{
-			sep = "";
-		}
-		switch (format[y])
-		{
-		case 'c':
-			printf("%c%s", va_arg(valist, int), sep);
-			break;
-		case 'x':
-			printf("%d%s", va_arg(valist, int), sep);
-			break;
-		case 'f':
-			printf("%f%s", va_arg(valist, double), sep);
-			break;
-		case 's':
-			str = va_arg(valist, char *);
-			if (str == NULL)
-				str = "(nil)";
-			printf("%s%s", str, sep);
-			break;
-		}
-		y++;
+		printf("(nil)");
+		return;
 	}
+	printf("%s", str);
+}
+
+/**
+ * print_all - check the code for Holberton School students.
+ * @format: number of arguments in character format
+ * Return: Nothing.
+ */
+
+void print_all(const char * const format, ...)
+{
+
+	va_list all;
+	unsigned int i, j;
+	char *separator = "";
+
+	f ops[] = {
+		{"c", op_c},
+		{"i", op_i},
+		{"f", op_f},
+		{"s", op_s},
+		};
+
+	va_start(all, format);
+	i = 0;
+	while (format && format[i])
+	{
+		j = 0;
+		while (j < 4)
+		{
+			if (ops[j].op[0] == format[i])
+			{
+				printf("%s", separator);
+				separator = ", ";
+				ops[j].f(all);
+				break;
+			}
+			j++;
+		}
+	i++;
+	}
+
 	printf("\n");
-	va_end(valist);
+	va_end(all);
 }
