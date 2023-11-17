59c1afd2e366dd6469600bdeef9520ec649cc2b2
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2023-11-16 16:23:33 -0800

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdlib.h>
+/**
+  * list_len - finds the number of nodes in a list
+  * @h: singly linked list
+  * Return: number of elements in a linked list
+  */
+size_t list_len(const list_t *h)
+{
+	int v;
+	for (v = 0; h; v++)
+		h = h->next;
+	return (v);
+}
