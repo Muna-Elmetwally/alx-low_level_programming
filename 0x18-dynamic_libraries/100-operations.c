69b515fd6cf2b5ce7296cb74d145bd844ac648e2
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2024-01-15 16:12:57 -0800

    dynamics

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,52 @@
+/**
+* mul - multiplies two integers
+* @i: first integer to add
+* @k: second integer to add
+* Return: the multiple
+*/
+
+int mul(int i, int k)
+{
+return (i * k);
+}
+
+/**
+* div - divideds two integers
+* @i: first integer to add
+* @k: second integer to add
+* Return: the dividend integer
+*/
+
+int div(int i, int k)
+{
+return (i / k);
+}
+
+/**
+*mod - finds the modulus of two integers
+*@i: first integer to add
+*@k: second integer to add
+*Return: the modulus
+*/
+
+int mod(int i, int k)
+{
+return (i % k);
+}
+
+/**
+* minus - minus of two integers
+* @i: first integer to add
+* @k: second integer to add
+* Return: the minused integer
+*/
+
+int sum(int i, int k)
+{
+return (i + k);
+}
+
+int minus(int i, int k)
+{
+return (i - k);
+}
