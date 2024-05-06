024c95a7cd3dfb5cb4ea791a474fd3b6196100f2
Muna-Elmetwally <munaelmetwally2397@gmail.com>
2024-05-06 23:22:09 +0300

    .

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,30 +1,82 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+
+int bin_search(int *array, size_t min, size_t max, int value);
+void print_array(int *array, size_t start, size_t end);
 
 /**
- * linear_search - linear cearch algorithm
+ * binary_search - search for an element using the binary search algorithm
  *
- * @array: pointer to an array of integers
- * @size: size for the array
- * @value: value to search for
+ * @array: haystack (int* array)
+ * @size: size of the haystack (size_t)
+ * @value: needle (int)
  *
- * Return: the index of the first match else -1 (Failure)
+ * Return: the index of first sucessful location else -1 (failure)
  */
-int linear_search(int *array, size_t size, int value)
+int binary_search(int *array, size_t size, int value)
 {
-	int idx;
 
-	if (!array)
+	if (!array || size == 0)
 		return (-1);
+	return (bin_search(array, 0, size - 1, value));
+}
 
-	idx = 0;
-	while (idx < (int) size)
-	{
-		printf("Value checked array[%d] = [%d]\n", idx, array[idx]);
-		if (array[idx] == value)
-			return (idx);
-		idx++;
-	}
+
+/**
+ * bin_search - recursively search for an
+ * element using the binary search algorithm
+ *
+ * @array: haystack (int* array)
+ * @min: of left side
+ * @max: index of right
+ * @value: needle (int)
+ *
+ * Return: the index of first sucessful location else -1 (failure)
+ */
+int bin_search(int *array, size_t min, size_t max, int value)
+{
+
+	size_t mid;
+
+	if (min > max)
+		return (-1);
+	if (min == 0)
+		mid = max / 2;
+	else
+		mid =  min + ((max - min) / 2);
+
+	print_array(array, min, max);
+	if (value == array[mid])
+		return ((int) mid);
+
+	if (value < array[mid])
+		return (bin_search(array, min, mid - 1, value));
+
+	if (value > array[mid])
+		return (bin_search(array, mid + 1, max, value));
 
 	return (-1);
+
+}
+
+/**
+ * print_array - prints array
+ *
+ * @array: haystack (int* array)
+ * @start: of left side
+ * @end: index of right
+ *
+ */
+void print_array(int *array, size_t start, size_t end)
+{
+	printf("Searching in array: ");
+	while (start <= end)
+	{
+		printf("%d", array[start]);
+		if (start < end)
+			printf(", ");
+		else
+			printf("\n");
+		start++;
+	}
 }
